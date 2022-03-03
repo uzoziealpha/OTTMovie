c10babb3d6130f031742c2674062745973a83410
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-03-04 07:32:36 +0800

    rendering search boc and Json DB

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  
+  const getMovieRequest = async(searchValue) => {
+     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=`;
+
+     const response = await fetch(url);
+     const responseJson = await response.json();
+
+
+     if (responseJson.Search){
+       setMovies(responseJson.Search);
+     }
+  };
+
+  useEffect(()=> {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+
+  return (
+    <div className="container-fluid movie-app">
+       <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading='Favourites'/>  
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+       </div>
+      <div className="row">
+        <MovieList 
+           movies={movies} 
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
